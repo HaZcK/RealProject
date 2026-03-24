1fc751e3bbd40071fb079b1850fea65b7c9a34dd
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-25 07:49:20 +0800

    Update ChatGll.lua

--- Project/ChatGlobal/ChatGll.lua
+++ Project/ChatGlobal/ChatGll.lua
@@ -1,4 +1,4 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
     Title = "ChatUi",
